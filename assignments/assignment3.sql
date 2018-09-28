39d4752ad38b7d040bad85138c32a66e83f72185
Unknown <rispir45@gmail.com>
2018-09-28 12:22:31 -0400

    Finished problem 1 of assignment 3

--- assignments/assignment3.sql
+++ assignments/assignment3.sql
@@ -8,4 +8,32 @@ DROP TABLE IF EXISTS acquaintances;
 DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS profiles;
 
-# Add your answers here
+CREATE TABLE profiles (
+	username VARCHAR(20) UNIQUE NOT NULL,
+    first VARCHAR(40),
+    last VARCHAR(40) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE messages (
+	id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+    sender VARCHAR(20) NOT NULL,
+    recipient VARCHAR(20) NOT NULL,
+    message VARCHAR(400) NOT NULL DEFAULT "",
+    is_read BOOLEAN NOT NULL DEFAULT FALSE,
+    sent_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    in_reply_to INT DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (sender)  REFERENCES profiles(username) ON DELETE CASCADE,
+    FOREIGN KEY (recipient)  REFERENCES profiles(username) ON DELETE CASCADE,
+    FOREIGN KEY (in_reply_to)  REFERENCES messages(id) ON DELETE SET NULL
+);
+
+CREATE TABLE acquaintances (
+	source VARCHAR(20) NOT NULL,
+    target VARCHAR(20) NOT NULL,
+    PRIMARY KEY (source, target),
+    FOREIGN KEY (source) REFERENCES profiles(username) ON DELETE CASCADE,
+    FOREIGN KEY (target) REFERENCES profiles(username) ON DELETE CASCADE
+);
+
