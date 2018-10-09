ef000e7cdb2c46f203ac8e1ef1a8e43fbe57032e
Unknown <rispir45@gmail.com>
2018-10-08 21:54:10 -0400

    Setup files for assignment 4

---
+++ assignments/assignment4.sql
@@ -0,0 +1,7 @@
+-- Dropping existing tables
+DROP TABLE IF EXISTS ev_invites;
+DROP TABLE IF EXISTS ev_events;
+DROP TABLE IF EXISTS ev_users;
+
+-- Your code goes here
+-- Use comments to separate each section
