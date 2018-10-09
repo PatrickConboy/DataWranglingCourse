9a24a05a7946acc8bb930a1c94c47392db4cdd66
Unknown <rispir45@gmail.com>
2018-10-08 22:37:10 -0400

    Finished problem 2 for assignment 4 (sql)

--- assignments/assignment4.sql
+++ assignments/assignment4.sql
@@ -12,3 +12,17 @@ CREATE TABLE ev_users (
     affiliation VARCHAR(40) DEFAULT "None",
     PRIMARY KEY (username)
 );
+
+-- Problem 2
+CREATE TABLE ev_events (
+	-- Again, made this unique, cause yea.
+    id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+    title VARCHAR(40) NOT NULL DEFAULT "",
+    longitude DECIMAL,
+    latitude DECIMAL,
+    owner VARCHAR(20) NOT NULL,
+    start TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    end TIMESTAMP NULL DEFAULT NULL,
+    FOREIGN KEY (owner) REFERENCES ev_users(username) ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
