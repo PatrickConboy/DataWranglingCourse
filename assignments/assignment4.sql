0696324bb322d4485668632d88d7284e81d4439b
Unknown <rispir45@gmail.com>
2018-10-08 22:22:19 -0400

    Finished problem 1 for assignment 4 (sql)

--- assignments/assignment4.sql
+++ assignments/assignment4.sql
@@ -3,5 +3,12 @@ DROP TABLE IF EXISTS ev_invites;
 DROP TABLE IF EXISTS ev_events;
 DROP TABLE IF EXISTS ev_users;
 
--- Your code goes here
--- Use comments to separate each section
+-- Problem 1
+CREATE TABLE ev_users (
+	-- I made username unique because I felt like it should be.
+	username VARCHAR(20) UNIQUE NOT NULL,
+    first VARCHAR(40),
+    last VARCHAR(40),
+    affiliation VARCHAR(40) DEFAULT "None",
+    PRIMARY KEY (username)
+);
