92783f1db48f8a4c8da7855c5ec109036f410bb0
Unknown <rispir45@gmail.com>
2018-10-25 21:41:08 -0400

    Preparing new files

---
+++ assignments/Finished Assignments/PatrickConboyAssignment4.py
@@ -0,0 +1,105 @@
+# Name:
+## Reading database keys
+import json
+with open('keys.json', 'r') as f:
+   account_info = json.loads(f.read())
+   vault = account_info['vault']
+
+from datetime import datetime, timedelta
+from sqlalchemy import *
+
+# The Status class is used in the enum column that you create in question 3
+# enum package documentation: https://docs.python.org/3/library/enum.html
+import enum
+class Status(enum.Enum):
+    Accepted = 1
+    Declined = 2
+    Maybe = 3
+
+engineString = 'mysql+mysqldb://{username}:{password}@{server}/{schema}'
+engineUrl = engineString.format(**vault)
+
+# Establishing a specific database connection
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+metadata = MetaData()
+
+# Adding our tables
+# Problem 1
+tblUsers = Table('ev_users', metadata,
+  Column('username', String(20), unique = True, primary_key = True),
+  Column('first', String(40)),
+  Column('last', String(40)),
+  Column('affiliation', String(40), default = 'None')
+)
+
+# Problem 2
+tblEvents = Table('ev_events', metadata,
+  Column('id', Integer, unique = True, nullable = False, primary_key = True, autoincrement = True),
+  Column('title', String(40), nullable = False, default = ""),
+  Column('longitude', Float(precision=32)),
+  Column('latitude', Float(precision=32)),
+  Column('owner', String(20),
+    ForeignKey("ev_users.username"),
+    nullable = False),
+  Column('start', DateTime, default = datetime.now()),
+  Column('end', DateTime, default = None)
+)
+
+# Problem 3
+tblInvites = Table('ev_invites', metadata,
+  Column('event_id', Integer,
+    ForeignKey("ev_events.id", ondelete="CASCADE"),
+    nullable = False),
+  Column('username', String(20),
+    ForeignKey("ev_users.username", ondelete="CASCADE"),
+    nullable = False),
+  Column('status', Enum(Status), nullable = True)
+)
+
+# Drop existing tables
+metadata.drop_all(engine)
+# Create these tables if they do not exist
+metadata.create_all(engine)
+
+conn = engine.connect()
+
+# Problem 4
+users = [
+  { "username": "conboyp", "first": "patrick", "last": "conboy", "affiliation": "Hanover College, Student" },
+  { "username": "jarnagink", "first": "kenny", "last": "jarnagin", "affiliation": "Hanover College, Student" },
+  { "username": "skiadasc", "first": "charilaos", "last": "skiadas", "affiliation": "Hanover College, Faculty, Staff" }
+]
+ins = tblUsers.insert()  # Create our insert object
+result = conn.execute(ins, users)  # Execute the insert on a connection
+
+# Problem 5
+event = [
+  {"title": "Homecoming get-together",
+   "longitude": 38.71,
+   "latitude": 85.46,
+   "owner": "conboyp",
+   "start": datetime(2018, 10, 6, 8, 0, 0)
+  }
+]
+ins = tblEvents.insert()  # Create our insert object
+result = conn.execute(ins, event)  # Execute the insert on a connection
+
+# Problem 6
+ins = tblInvites.insert().\
+from_select ([tblInvites.c.event_id, tblInvites.c.username, tblInvites.c.status],
+  select([tblInvites.c.event_id, tblEvents.c.owner, literal("Accepted")]).\
+  where(tblEvents.c.owner != tblInvites.c.username))
+
+# Problem 7
+ins = tblInvites.insert().\
+from_select ([tblInvites.c.event_id, tblInvites.c.username],
+  select([tblEvents.c.id, tblUsers.c.username]).\
+  where(tblUsers.c.affiliation.like('\%Hanover College\%')).\
+  where(tblEvents.c.title.like('\%Homecoming\%')))
+
+# Problem 8
+upd = tblEvents.update().values(
+  end = timedelta(hours=tblEvents.c.start + 2).\
+  where(tblEvents.c.end < tblEvents.c.start))
+# Couldn't get number 8 working, but I think I'm close???
---
+++ assignments/Finished Assignments/PatrickConboyAssignment4.sql
@@ -0,0 +1,76 @@
+-- Dropping existing tables
+DROP TABLE IF EXISTS ev_invites;
+DROP TABLE IF EXISTS ev_events;
+DROP TABLE IF EXISTS ev_users;
+
+-- Problem 1
+CREATE TABLE ev_users (
+	-- I made username unique because I felt like it should be.
+	username VARCHAR(20) UNIQUE NOT NULL,
+    first VARCHAR(40),
+    last VARCHAR(40),
+    affiliation VARCHAR(40) DEFAULT "None",
+    PRIMARY KEY (username)
+);
+
+-- Problem 2
+CREATE TABLE ev_events (
+	-- Again, made this unique, cause yea.
+    id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+    title VARCHAR(40) NOT NULL DEFAULT "",
+    longitude DECIMAL(6,2),
+    latitude DECIMAL(6,2),
+    owner VARCHAR(20) NOT NULL,
+    start TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    end TIMESTAMP NULL DEFAULT NULL,
+    FOREIGN KEY (owner) REFERENCES ev_users(username),
+    PRIMARY KEY (id)
+);
+
+-- Problem 3
+CREATE TABLE ev_invites (
+	event_id INTEGER NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    status ENUM ('Accepted', 'Declined', 'Maybe'),
+    FOREIGN KEY (username) REFERENCES ev_users(username) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES ev_events(id) ON DELETE CASCADE
+);
+
+-- Problem 4
+INSERT INTO ev_users (username, first, last, affiliation) VALUES 
+	("conboyp", "patrick", "conboy", "Hanover College, Student"),
+	("jarnagink", "kenny", "jarnagin", "Hanover College, Student"),
+    ("skiadasc", "charilaos", "skiadas", "Hanover College, Faculty, Staff");
+    
+-- Problem 5
+INSERT INTO ev_events (title, longitude, latitude, owner, start) VALUES
+	("Homecoming get-together", 38.71, 85.46, "conboyp", '2018-10-6 08:00:00');
+    
+-- Problem 6
+INSERT INTO ev_invites (event_id, username, status) 
+SELECT e.id, e.owner, 'Accepted'
+FROM ev_events AS e
+WHERE NOT EXISTS (SELECT i.username
+				  FROM ev_invites AS i, ev_events AS e
+                  WHERE e.owner = i.username);
+                  
+-- Problem 7
+INSERT INTO ev_invites (event_id, username)
+SELECT e.id, u.username
+FROM ev_events AS e, ev_users AS u
+WHERE e.title LIKE "%Homecoming%"
+AND u.affiliation LIKE "%Hanover College%";
+
+-- Problem 8
+UPDATE ev_events SET end = DATE_ADD(start, INTERVAL 2 HOUR)
+	WHERE (end is NULL or end < start)
+    AND id > 0;
+    
+-- Problem 9
+UPDATE ev_events SET start = DATE_ADD(start, INTERVAL 1 DAY)
+	WHERE id in (SELECT event_id
+				 FROM ev_invites
+                 WHERE status = 'Accepted'
+                 GROUP BY event_id
+                 HAVING COUNT(status) < 5)
+	AND ev_events.id > 0;
